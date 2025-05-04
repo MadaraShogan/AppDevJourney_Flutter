825fe3748cf3f8cc3bf0d10bc90a7293893b9758
MadaraShogan <deepanshusingh2812@gmail.com>
2025-05-04 11:45:06 +0530

    Basic variable,operator etc, 1:00:29

--- Dart/1_Basic.dart
+++ Dart/1_Basic.dart
@@ -1,3 +1,16 @@
 void main() {
   print('Hello, World!');
+  print(5);
+  print('5'+'6');
+  print('5'*6);
+  bool isTrue=true;            //in java -> boolean
+  print(isTrue);
+  dynamic something="100";     //Can be Anytype (will change accordingly)
+  print(something);
+
+  String greet="Hello";
+  greet= "${greet.length} World";
+  print(greet);
+
+  // < var/fina/const >  <varnName> = value;
 }
